f811e10704131cef7c3552cf16483794aff2a38e
jjnanthakumar <jjnanthakumar477@gmail.com>
2021-01-21 20:12:26 +0530

    updated

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,14 @@
 import User from "../models/user.js";
-import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import moment from 'moment';
 
 export const signIn = async (req, res) => {
     const { email, password } = req.body;
     try {
         const existingUser = await User.findOne({ email })
-        if (!existingUser) return res.status(404).json({ message: "User doesn't exists! Please register first :)" })
+        if (!existingUser) return res.json({ message: "Please register first :)" })
         const isPasscheck = await bcrypt.compare(password, existingUser.password);
-        if (!isPasscheck) return res.status(400).json({ message: "Incorrect Password!" })
+        if (!isPasscheck) return res.json({ message: "Incorrect Password!" })
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.SECRET, { expiresIn: "1h" })
         res.status(200).json({ result: existingUser, token })
     } catch (err) {
@@ -22,8 +20,8 @@ export const signUp = async (req, res) => {
     const { firstName, lastName, email, password, confirmpass } = req.body;
     try {
         const existingUser = await User.findOne({ email })
-        if (existingUser) return res.status(400).json({ message: "User already exists! Please login :)" })
-        if (password !== confirmpass) return res.status(400).json({ message: "Passwords doesn't match!" })
+        if (existingUser) return res.json({ message: "Email already exists! Please login :)" })
+        if (password !== confirmpass) return res.json({ message: "Passwords doesn't match!" })
         const hashedPass = await bcrypt.hash(password, 12)
         const result = await User.create({ email, password: hashedPass, name: `${firstName} ${lastName}` })
         const token = jwt.sign({ email: res.email, id: res._id }, process.env.SECRET, { expiresIn: "1h" })
