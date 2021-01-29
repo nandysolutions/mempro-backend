43585c58be9caa79a0ebeb190924f380639d58da
jjnanthakumar <jjnanthakumar477@gmail.com>
2021-01-29 20:06:51 +0530

    upd

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -3,17 +3,23 @@ import decode from 'jwt-decode'
 const auth = (req, res, next) => {
     try {
         let decodedData;
-        const token = req.headers.authorization.split(' ')[1];
+        const headers = req.headers
+        const token = headers.authorization.split(' ')[1];
         const isCutomtoken = token.length < 500;
-        if (token && isCutomtoken) {
+        if (headers.type === 'github') {
+            const user = req.headers.id;
+            req.userId = user
+
+        } else if (token && isCutomtoken) {
             decodedData = jwt.decode(token, process.env.SECRET)
             req.userId = decodedData?.id;
         }
         else {
             decodedData = decode(token);
             if (decodedData?.sub === undefined) {
-                req.userId = decodedData.user_id
-            } else {
+                req.userId = decodedData?.user_id
+            }
+            else {
                 req.userId = decodedData?.sub;
             }
         }
--- controllers/posts.js
+++ controllers/posts.js
@@ -69,7 +69,7 @@ export const commentPost = async (req, res) => {
 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
     const cur_post = await Post.findById(id);
-    cur_post.comments.push({ id: req.userId, commentAt: new Date().toISOString(), comment: req.body.comment, c_id: newId2, name: req.body.user.name, imageUrl: req.body.user.imageUrl || req.body.user.picture.data.url })
+    cur_post.comments.push({ id: req.userId, commentAt: new Date().toISOString(), comment: req.body.comment, c_id: newId2, name: req.body.user.name, imageUrl: req.body.user?.imageUrl || req.body.user?.picture?.data.url || req.body.user?.avatar_url })
     const upd_post = await Post.findByIdAndUpdate(id, cur_post, { new: true });
     res.status(201).json(upd_post)
 }
